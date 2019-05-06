540837c7eafa3b3203b9fd864aef3d149bea4916
Yev <yevster@gmail.com>
2019-05-06 19:41:00 -0400

    Update setup.ps1
    Adding Desktop Shortcut.

--- setup.ps1
+++ setup.ps1
@@ -2,3 +2,12 @@
 Invoke-WebRequest https://go.microsoft.com/fwlink/?Linkid=850640 -OutFile "${env:TEMP}/vscode.zip"
 $archive = Get-Item "${env:TEMP}/vscode.zip"
 Expand-Archive -Path $archive -DestinationPath "C:\vscode"
+
+$codeExe=gi 'C:\vscode\Code*.exe'
+$target="$(gi ~)/Desktop/Code - Insiders.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($target)
+$Shortcut.TargetPath = $codeExe
+$Shortcut.Save()
+
+
